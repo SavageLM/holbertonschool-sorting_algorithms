bb5ea6fc753ab2bb8aa61c6c47e303abb98cca75
SavageLM <nagol.egavas@gmail.com>
2023-08-14 18:09:24 -0700

    Added 0-bubble_sort file and code as well as header and README

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the readme. I dont have time to do a real one right  now.
---
+++ replace.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * replace - swaps two integers
+ * @a: first integer
+ * @b: second integer
+ */
+
+void replace(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
